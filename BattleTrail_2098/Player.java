0c20fc193c2424584ba6218c9ae769cf89db18f0
Yazpants <Victoriusify@gmail.com>
2017-02-22 20:47:50 +0100

    Stats for now

--- BattleTrail_2098/Player.java
+++ BattleTrail_2098/Player.java
@@ -12,6 +12,19 @@ public class Player {
     }
 
     private void playerStats() {
+        //Main stats
+        double str;
+        double intl;
+        double health;
+        double luck;
+
+        // extra weapon stats
+        int plasma_rifles;
+        int rifles;
+        int shields;
+        int hand_to_hand_combat_weapons;
+        int melee_weapons;
+
     }
 
     private void inventorySystem() {
